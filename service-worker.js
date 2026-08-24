37a9fe78c558d68e38c239f8cf4879930ab28eeb
AbhishekBhovar <abhishekbhovar@gmail.com>
2026-08-24 11:01:49 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,1 @@
-const CACHE='hp-fitness-rpg-v49';
-self.addEventListener('install',event=>{self.skipWaiting();});
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
-});
+const CACHE='hp-fitness-rpg-v50';self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));});
