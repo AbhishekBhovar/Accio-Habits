186fec87f27cf908dfe63f0c9e92171cb2ab9258
AbhishekBhovar <abhishekbhovar@gmail.com>
2026-08-25 20:17:11 +1000

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-const CACHE='hp-slim-fix-0825';self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));});
